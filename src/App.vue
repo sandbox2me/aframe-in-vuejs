93b79934d03cb6857391530db93cba5fa8782a7b
thoragio <thoragio@gmail.com>
2016-12-12 11:26:38 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="container">
+  <div class="title">
+      <h1>
+        {{ msg }}
+      </h1>
+    </div>
+  <div class="ascene" v-html="ascene">
+  </div>
+</div>
+</template>
+
+<script>
+require('aframe');
+
+export default {
+    data() {
+        return {
+            msg: 'Basic A-frame scene rendered in Vue.js',
+            ascene:
+            '<a-scene>\
+                <a-sphere color="#999" radius="2" position="0 4 -15"></a-sphere>\
+            </a-scene>'
+        }
+    }
+}
+</script>
+
+<style>
+.title {
+    color: #333;
+    text-align: center;
+    font-family: sans-serif;
+}
+</style>
